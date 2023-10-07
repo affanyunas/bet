e033f6d95f7cc5c57b550c83bc9dfe3ac6cffc6b
affanyunas <74919708+affanyunas@users.noreply.github.com>
2023-10-07 11:19:34 +0700

    Add number format

--- bet.bat
+++ bet.bat
@@ -2,9 +2,9 @@
 setlocal enabledelayedexpansion
 
 :loop
-set /p bet="Masukkan jumlah bet : "
-set /p lose="Masukkan jumlah lose : "
-set /p persenlose="Masukkan persentase lose : "
+set /p bet="Masukkan jumlah Bet : "
+set /p lose="Masukkan jumlah Lose : "
+set /p persenlose="Masukkan persentase Lose : "
 
 set hasil=!bet!
 
@@ -16,32 +16,33 @@ set /a lose-=1
 goto calculate
 
 :end
-echo Hasil akhir: %hasil%
 
-choice /c yn /m "Mau menghitung lagi? (y/n): "
-if errorlevel 2 goto exit
-if errorlevel 1 goto loop
-
-:exit
-@echo off
+:: Mengubah format hasil akhir dengan pemisahan ribuan seperti Excel (contoh: Rp 21.600)
 setlocal enabledelayedexpansion
+set "formattedResult="
+set "result=!hasil!"
+set "count=0"
 
-:loop
-set /p bet="Masukkan jumlah bet (misal: 100): "
-set /p lose="Masukkan jumlah lose (misal: 1): "
-set /p persenlose="Masukkan persentase lose (misal: 500%): "
+:formatResult
+set /a "count+=1"
+set "digit=!result:~-1!"
+set "result=!result:~0,-1!"
+set "formattedResult=!digit!!formattedResult!"
 
-set hasil=!bet!
+if not "!result!"=="" (
+  if !count! equ 3 (
+    set "formattedResult=.!formattedResult!"
+    set "count=0"
+  )
+  goto formatResult
+)
 
-:calculate
-if %lose% leq 0 goto end
+if %count% gtr 0 (
+  set "formattedResult=!formattedResult!"
+)
 
-set /a hasil=!hasil! + (!hasil! * %persenlose:/=%% / 100)
-set /a lose-=1
-goto calculate
-
-:end
-echo Hasil akhir: %hasil%
+set hasilFormatted=Rp !formattedResult!
+echo Hasil akhir: %hasilFormatted%
 
 set /p choice="Mau menghitung lagi? (y/n): "
 if "%choice%" == "n" goto exit
